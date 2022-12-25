2a7af760e76ad0e9aa9a6b6cceaeb3d2d41fb232
JC <courier@pop-os.localdomain>
2022-12-25 16:05:16 -0500

    initial commit

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+class_name Actor
+
+var FLOOR_NORMAL := Vector2.UP
+export var speed := Vector2(300.0, 1000.0)
+export var gravity := 4000.0
+
+var velocity := Vector2.ZERO #:= Means set the type to the right hand side
+
+
+
+#func _physics_process(delta: float) -> void:
+
+    
+
