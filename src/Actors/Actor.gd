71d78fd0359cdffc6e40a6eec180bb4aac861ef0
jdclifton2 <jdclifton2@catamount.wcu.edu>
2022-12-25 21:35:40 -0500

    cleaned up code

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -5,7 +5,7 @@ var FLOOR_NORMAL := Vector2.UP
 export var speed := Vector2(300.0, 1000.0)
 export var gravity := 4000.0
 
-var velocity := Vector2.ZERO #:= Means set the type to the right hand side
+var _velocity := Vector2.ZERO #:= Means set the type to the right hand side
 
 
 
